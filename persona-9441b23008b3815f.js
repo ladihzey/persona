2b7d37c0fbcb3f33a2cb2906a93d391448514c07
ladihzey <ladihzey@users.noreply.github.com>
2023-05-24 23:02:07 +0000

    deploy: b8e9aa1bd6f550ac7c49d1fa9bff812bbea061ff

--- persona-9441b23008b3815f.js
+++ persona-9441b23008b3815f.js
@@ -560,7 +560,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('persona-57069feac76cb7e_bg.wasm', import.meta.url);
+        input = new URL('persona-9441b23008b3815f_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
