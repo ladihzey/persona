ee0c8c5123945020259e6aa62c3ec3e04000ecb8
ladihzey <ladihzey@proton.me>
2023-08-09 02:18:31 +0200

    feat: add profile picture

--- src/app/components/intro_section.rs
+++ src/app/components/intro_section.rs
@@ -3,7 +3,10 @@ use leptos::*;
 #[component]
 pub fn IntroSection(cx: Scope) -> impl IntoView {
     view! { cx,
-        <main class="min-h-screen w-screen bg-gray-800 flex items-center justify-center">
+        <main class="min-h-screen w-screen bg-gray-800 flex items-center justify-center gap-8">
+            <div class="relative w-48 h-48 overflow-hidden rounded-full border-8 border-gray-700">
+                <img src="assets/profile-photo.webp" alt="Profile photo" class="absolute inset-0 w-full h-full object-cover" />
+            </div>
             <h1 class="text-stone-200 text-5xl">
                 "Hi, I'm Yegor Zhidal"
             </h1>
