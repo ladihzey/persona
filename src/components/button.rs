44d536e7a5b1870116b8e99610b02f3f1fbe2bbd
ladihzey <ladihzey@proton.me>
2023-08-11 23:12:37 +0200

    feat: simplify ui looks and add background to the page

--- src/components/button.rs
+++ src/components/button.rs
@@ -3,7 +3,7 @@ use leptos::*;
 #[component]
 pub fn Button(cx: Scope, children: Children) -> impl IntoView {
     view! { cx,
-        <button class="text-gray-800 bg-emerald-400 hover:bg-emerald-600 flex gap-2 font-bold py-2 px-4 rounded">
+        <button class="text-neutral-900 bg-teal-500 hover:bg-teal-700 flex gap-2 font-bold py-2 px-4 rounded">
             {children(cx)}
         </button>
     }
